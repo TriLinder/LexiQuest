1b3115486a146ca8aa13a6b2151f7d200d53b151
TriLinder <82846064+TriLinder@users.noreply.github.com>
2023-02-04 13:44:05 +0100

    Implement letter score multipliers

--- python-scripts/generate_get_tile_value.py
+++ python-scripts/generate_get_tile_value.py
@@ -8,7 +8,11 @@ def generate_get_tile_value() -> None:
     output = "" 
     
     for letter in letters:
-        output += f"execute if block ~ ~ ~ {letter['block']} run scoreboard players set @s tile_value {letter['value']}\n"
+        output += f"execute if block ~ ~ ~ {letter['block']} run scoreboard players set @s tile_value {letter['value']} \n"
+
+    #Special tiles
+    output += "execute if score @s tile_type matches 1 run scoreboard players operation @s tile_value *= 2 numerals \n" #Double letter score
+    output += "execute if score @s tile_type matches 2 run scoreboard players operation @s tile_value *= 3 numerals \n" #Triple letter score
 
     with open(Path("output/get_tile_value.mcfunction"), "w") as f:
         f.write(output)
